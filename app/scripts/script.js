fed80936605f51c93f8d6548387add668c629503
Leonid Marchenko <codobred@gmail.com>
2022-05-12 19:52:35 +0300

    make basic support for touch

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,21 +1,20 @@
-
 /* --------------------
 
-   GENERAL FUNCTIONS   
+   GENERAL FUNCTIONS
 
 -------------------- */
 
 function $(e) {
-  return document.querySelectorAll(e)[1] === undefined ? document.querySelector(e) : document.querySelectorAll(e)
+    return document.querySelectorAll(e)[1] === undefined ? document.querySelector(e) : document.querySelectorAll(e)
 }
 
-function addEventListener(element, events, handler) {
-  events.trim().split(" ").forEach(event => {
-    element.addEventListener(event, handler);
-  });
+function attachEventListener(element, events, handler) {
+    events.trim().split(" ").forEach(event => {
+        element.addEventListener(event, handler);
+    });
 }
 
-Array.prototype.shuffle = function() {
+Array.prototype.shuffle = function () {
     for (let i = this.length; i; i--) {
         let j = Math.floor(Math.random() * i);
         [this[i - 1], this[j]] = [this[j], this[i - 1]]
@@ -40,108 +39,106 @@ let offsetY = 0
 -------------------- */
 
 class Card {
-  constructor(num, type) {
-    this.num = num
-    this.type = type
-  }
-  
-  get numName() {
-    return this._numToName(this.num)
-  }
-
-  get typeName() {
-    return this._typeToName(this.type)
-  }
-
-  get element() {
-    let el = document.createElement('div')
-    el.classList.add('card')
-    el.classList.add(this.typeName[2])
-    el.setAttribute("data-num", this.num)
-    el.setAttribute("data-type", this.type)
-    const span = `<span>${this.numName} ${this.typeName[1]}</span>`
-    el.innerHTML = `${span}<div class="type">${this.typeName[1]}</div>${span}`
-    return el
-  }
-
-  _numToName(num) {
-    if (num >= 1 && num <= 13) {
-      switch (num) {
-        case 1:
-          return 'A'
-        case 11:
-          return 'J'
-        case 12:
-          return 'Q'
-        case 13:
-          return 'K'
-        default:
-          return num
-      }
-    } else {
-      throw new Error(`Num '${this.num}' does not exist`)
+    constructor(num, type) {
+        this.num = num
+        this.type = type
     }
-  }
 
-  _typeToName(type) {
-    switch (type) {
-      case 0:
-        return ['Hearts', '♥', 'red']
-      case 1:
-        return ['Clubs', '♣', 'black']
-      case 2:
-        return ['Diamonds', '♦', 'red']
-      case 3:
-        return ['Spades', '♠', 'black']
-      default:
-        throw new Error(`Card type '${type}' does not exist`)
+    get numName() {
+        return this._numToName(this.num)
     }
-  }
+
+    get typeName() {
+        return this._typeToName(this.type)
+    }
+
+    get element() {
+        let el = document.createElement('div')
+        el.classList.add('card')
+        el.classList.add(this.typeName[2])
+        el.setAttribute("data-num", this.num)
+        el.setAttribute("data-type", this.type)
+        const span = `<span>${this.numName} ${this.typeName[1]}</span>`
+        el.innerHTML = `${span}<div class="type">${this.typeName[1]}</div>${span}`
+        return el
+    }
+
+    _numToName(num) {
+        if (num >= 1 && num <= 13) {
+            switch (num) {
+                case 1:
+                    return 'A'
+                case 11:
+                    return 'J'
+                case 12:
+                    return 'Q'
+                case 13:
+                    return 'K'
+                default:
+                    return num
+            }
+        } else {
+            throw new Error(`Num '${this.num}' does not exist`)
+        }
+    }
+
+    _typeToName(type) {
+        switch (type) {
+            case 0:
+                return ['Hearts', '♥', 'red']
+            case 1:
+                return ['Clubs', '♣', 'black']
+            case 2:
+                return ['Diamonds', '♦', 'red']
+            case 3:
+                return ['Spades', '♠', 'black']
+            default:
+                throw new Error(`Card type '${type}' does not exist`)
+        }
+    }
 }
 
 function createCards() {
-  for (let i = 0; i <= 3; i++) {
-    for (let j = 1; j <= 13; j++) {
-      allCards.push(new Card(j,i))
+    for (let i = 0; i <= 3; i++) {
+        for (let j = 1; j <= 13; j++) {
+            allCards.push(new Card(j, i))
+        }
     }
-  }
-  allCards.shuffle();
+    allCards.shuffle();
 }
 
 function initStacks() {
-  for (let i = 1; i < 8; i++) {
-    let el = document.createElement("div")
-    el.classList.add("stack")
-    el.classList.add("stack_" + i)
-    el.setAttribute("data-max", i)
-    $('.stacks').appendChild(el)
-  }
-
-  for (let el of $(".stack")) {
-    let max = el.getAttribute("data-max")
-
-    for (let j = 0; j < max; j++) {
-      let card = allCards[0]
-      allCards.splice(0, 1)
-      let newEl = card.element
-      if (j == max - 1) {
-        newEl.classList.add("open")
-      }
-      el.appendChild(newEl)
+    for (let i = 1; i < 8; i++) {
+        let el = document.createElement("div")
+        el.classList.add("stack")
+        el.classList.add("stack_" + i)
+        el.setAttribute("data-max", i)
+        $('.stacks').appendChild(el)
     }
 
-    addEventListener(el, "mouseup touchend", function() {
-      console.log('[selectedElements]', selectedElements);
+    for (let el of $(".stack")) {
+        let max = el.getAttribute("data-max")
 
-      if (typeof selectedElements[0] !== "undefined") {
-        onDrop(this)
-      }
-    })
-  }
+        for (let j = 0; j < max; j++) {
+            let card = allCards[0]
+            allCards.splice(0, 1)
+            let newEl = card.element
+            if (j == max - 1) {
+                newEl.classList.add("open")
+            }
+            el.appendChild(newEl)
+        }
 
-  for (let card of allCards) {
-    $(".full").appendChild(card.element)
-  }
+        attachEventListener(el, "mouseup", function () {
+            if (typeof selectedElements[0] !== "undefined") {
+                onDrop(this)
+            }
+        })
+    }
+
+    for (let card of allCards) {
+        $(".full").appendChild(card.element)
+    }
 }
 
 /* --------------------
@@ -151,27 +148,37 @@ function initStacks() {
 -------------------- */
 
 function _addSelectedElement(el) {
-  el.classList.add("selected")
-  selectedElements.push(el)
+    el.classList.add("selected")
+    selectedElements.push(el)
 }
 
 function _rmSelectedElements() {
-  if (typeof selectedElements[0] !== undefined) {
-    for (let el of selectedElements) {
-      el.classList.remove("selected")
+    if (typeof selectedElements[0] !== undefined) {
+        for (let el of selectedElements) {
+            el.classList.remove("selected")
+        }
+        selectedElements = []
     }
-    selectedElements = []
-  }
+}
+
+function resetSelected() {
+    if (typeof selectedElements[0] !== undefined) {
+        for (let el of selectedElements) {
+            el.style.top = "auto"
+            el.style.left = "auto"
+        }
+    }
+    _rmSelectedElements()
 }
 
 function _moveElement(element, toParent, toFront = false) {
-  let element2 = element
-  element.parentNode.removeChild(element)
-  if (toFront) {
-    toParent.insertBefore(element2, toParent.firstChild)
-  } else {
-    toParent.appendChild(element2)
-  }
+    let element2 = element
+    element.parentNode.removeChild(element)
+    if (toFront) {
+        toParent.insertBefore(element2, toParent.firstChild)
+    } else {
+        toParent.appendChild(element2)
+    }
 }
 
 /* --------------------
@@ -180,144 +187,190 @@ function _moveElement(element, toParent, toFront = false) {
 
 -------------------- */
 
+function updateElementPosition(element, event) {
+    const clientX = event.touches ? event.touches[0].pageX : event.clientX;
+    const clientY = event.touches ? event.touches[0].pageY : event.clientY;
+
+    element.style.top = (clientY - offsetY) + "px"
+    element.style.left = (clientX - offsetX) + "px"
+}
+
 function initDragAndDrop() {
-  for (let cardsEl of $(".card")) {
-    addEventListener(cardsEl, "mousedown touchstart", function(e) {
-      offsetX = e.offsetX
-      offsetY = e.offsetY
-      if (this.parentNode.lastChild == this) {
-        this.classList.add("open")
-      }
-      if (this.classList.contains("open")) {
-        let sibling = this
-        while(sibling) {
-          _addSelectedElement(sibling)
-          sibling = sibling.nextElementSibling
+    for (let cardsEl of $(".card")) {
+        attachEventListener(cardsEl, "mousedown touchstart", function (e) {
+            const rect = e.target.getBoundingClientRect();
+
+            offsetX = e.touches ? (e.touches[0].clientX - rect.left) : e.offsetX;
+            offsetY = e.touches ? (e.touches[0].clientY - rect.top) : e.offsetY;
+
+            if (this.parentNode.lastChild == this) {
+                this.classList.add("open")
+            }
+
+            if (this.classList.contains("open")) {
+                let sibling = this
+                while (sibling) {
+                    _addSelectedElement(sibling)
+                    sibling = sibling.nextElementSibling
+                }
+            }
+            for (let el of selectedElements) {
+                updateElementPosition(el, e);
+            }
+        })
+    }
+
+    document.addEventListener("dblclick", autoMove)
+
+    attachEventListener(document, "mouseup", function () {
+        resetSelected();
+    })
+
+    attachEventListener(document, "mousemove touchmove", function (ev) {
+        if (typeof selectedElements[0] !== undefined) {
+            for (let el of selectedElements) {
+                updateElementPosition(el, ev);
+            }
         }
-      }
-      for (let el of selectedElements) {
-        el.style.top = event.clientY - offsetY + "px"
-        el.style.left = event.clientX - offsetX + "px"
-      }
     })
-  }
 
-  document.addEventListener("dblclick", autoMove)
-
-  addEventListener(document, "mouseup touchend", function() {
-    if (typeof selectedElements[0] !== undefined) {
-      for (let el of selectedElements) {
-        el.style.top = "auto"
-        el.style.left = "auto"
-      }
+    for (let el of $(".foundations")) {
+        attachEventListener(el, "mouseup", function () {
+            onDrop(this)
+        });
     }
-    _rmSelectedElements()
-  })
 
-  addEventListener(document, "mousemove touchmove", function() {
-    if (typeof selectedElements[0] !== undefined) {
-      for (let el of selectedElements) {
-        el.style.top = event.clientY - offsetY + "px"
-        el.style.left = event.clientX - offsetX + "px"
-      }
-    }
-  })
-  
-  for (let el of $(".foundations")) {
-    addEventListener(el, "mouseup touchend", function() {
-      onDrop(this)
+    attachEventListener(document, "touchend", function (event) {
+        const {pageX, pageY} = event.changedTouches[0];
+
+        const acceptableSelectors = ['foundations', 'stack'];
+
+        const element = (() => {
+            let el = document.elementFromPoint(pageX, pageY);
+
+            const hasAnyClassCheck = () => {
+                return acceptableSelectors.some((className) => el.classList.contains(className));
+            };
+
+            do {
+                if (!el) {
+                    break;
+                }
+
+                if (hasAnyClassCheck()) {
+                    return el;
+                }
+
+                el = el.parentElement;
+            } while (true);
+
+            return el;
+        })();
+
+        if (element) {
+            if (element.classList.contains('foundations')) {
+                onDrop(element);
+            }
+
+            if (element.classList.contains('stack')) {
+                onDrop(element);
+            }
+        }
+
+        resetSelected();
+    });
+
+    $(".full").addEventListener("click", function () {
+        if (this.childNodes.length > 0) {
+            this.lastChild.classList.add("open")
+            _moveElement(this.lastChild, $(".view"))
+        } else {
+            for (let card of $(".view .card")) {
+                card.classList.remove("open")
+                _moveElement(card, $(".full"), true)
+            }
+        }
     })
-  }
-
-  $(".full").addEventListener("click", function() {
-    if (this.childNodes.length > 0) {
-      this.lastChild.classList.add("open")
-      _moveElement(this.lastChild, $(".view"))
-    } else {
-      for (let card of $(".view .card")) {
-        card.classList.remove("open")
-        _moveElement(card, $(".full"), true)
-      }
-    }
-  })
 }
 
 function onDrop(parent) {
-  if (typeof selectedElements[0] !== undefined) {
-    if (selectedElements.length + parent.childNodes.length <= 13) {
-      if (parent.classList.contains("foundations")) {
-        if (foundationCanStack(parent)) {
-          allowDrop(parent)
-        }
-      }
-      if (parent.classList.contains("stack")) {
-        if (stacksCanStack(parent)) {
-          allowDrop(parent)
-        }
-      }
+    if (!selectedElements.length) {
+        return;
     }
-  }
+
+    if (selectedElements.length + parent.childNodes.length <= 13) {
+        if (parent.classList.contains("foundations")) {
+            if (foundationCanStack(parent)) {
+                allowDrop(parent)
+            }
+        }
+        if (parent.classList.contains("stack")) {
+            if (stacksCanStack(parent)) {
+                allowDrop(parent)
+            }
+        }
+    }
 }
 
 function allowDrop(parent) {
-  for (let el of selectedElements) {
-    let element = el
-    el.parentNode.removeChild(el)
+    for (let el of selectedElements) {
+        let element = el
+        el.parentNode.removeChild(el)
 
-    parent.appendChild(element)
+        parent.appendChild(element)
 
-    element.style.top = "auto"
-    element.style.left = "auto"
-  }
-  _rmSelectedElements()
+        element.style.top = "auto"
+        element.style.left = "auto"
+    }
+    _rmSelectedElements()
 }
 
 function foundationCanStack(parent) {
-  if (parent.childNodes.length === 0) {
-    return (parseInt(selectedElements[0].getAttribute("data-num")) === 1)
-  }
+    if (parent.childNodes.length === 0) {
+        return (parseInt(selectedElements[0].getAttribute("data-num")) === 1)
+    }
 
-  if (selectedElements.length > 1) {
-    return false
-  }
+    if (selectedElements.length > 1) {
+        return false
+    }
 
-  if (parseInt(parent.lastChild.getAttribute("data-type")) === parseInt(selectedElements[0].getAttribute("data-type"))) {
-    const num1 = parseInt(parent.lastChild.getAttribute("data-num"))
-    const num2 = parseInt(selectedElements[0].getAttribute("data-num"))
-    return (num1 + 1 === num2)
-  }
+    if (parseInt(parent.lastChild.getAttribute("data-type")) === parseInt(selectedElements[0].getAttribute("data-type"))) {
+        const num1 = parseInt(parent.lastChild.getAttribute("data-num"))
+        const num2 = parseInt(selectedElements[0].getAttribute("data-num"))
+        return (num1 + 1 === num2)
+    }
 }
 
 function stacksCanStack(parent) {
-  if (parent.childNodes.length === 0) {
-    return (parseInt(selectedElements[0].getAttribute("data-num")) === 13)
-  }
+    if (parent.childNodes.length === 0) {
+        return (parseInt(selectedElements[0].getAttribute("data-num")) === 13)
+    }
 
-  if (parseInt(parent.lastChild.getAttribute("data-num")) - 1 === parseInt(selectedElements[0].getAttribute("data-num"))) {
-    const type1 = parseInt(parent.lastChild.getAttribute("data-type")) % 2
-    const type2 = parseInt(selectedElements[0].getAttribute("data-type")) % 2
-    return (type1 !== type2)
-  }
+    if (parseInt(parent.lastChild.getAttribute("data-num")) - 1 === parseInt(selectedElements[0].getAttribute("data-num"))) {
+        const type1 = parseInt(parent.lastChild.getAttribute("data-type")) % 2
+        const type2 = parseInt(selectedElements[0].getAttribute("data-type")) % 2
+        return (type1 !== type2)
+    }
 }
 
 function autoMove() {
-  for (let stack of $('.stack')) {
-    for (let foundation of $(".foundations")) {
-      if (stack.childNodes.length > 0) {
-        if (foundation.childNodes.length > 0) {
-          if (stack.lastChild.getAttribute("data-type") === foundation.lastChild.getAttribute("data-type")) {
-            if (parseInt(stack.lastChild.getAttribute("data-num")) === parseInt(foundation.lastChild.getAttribute("data-num")) + 1) {
-              _moveElement(stack.lastChild, foundation)
+    for (let stack of $('.stack')) {
+        for (let foundation of $(".foundations")) {
+            if (stack.childNodes.length > 0) {
+                if (foundation.childNodes.length > 0) {
+                    if (stack.lastChild.getAttribute("data-type") === foundation.lastChild.getAttribute("data-type")) {
+                        if (parseInt(stack.lastChild.getAttribute("data-num")) === parseInt(foundation.lastChild.getAttribute("data-num")) + 1) {
+                            _moveElement(stack.lastChild, foundation)
+                        }
+                    }
+                } else {
+                    if (stack.lastChild.getAttribute("data-num") == 1) {
+                        _moveElement(stack.lastChild, foundation)
+                    }
+                }
             }
-          }
-        } else {
-          if (stack.lastChild.getAttribute("data-num") == 1) {
-            _moveElement(stack.lastChild, foundation)
-          }
         }
-      }
     }
-  }
 }
 
 /* --------------------
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,51 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	nano = require('gulp-cssnano'),
-	prefix = require('gulp-autoprefixer'),
-	babel = require('gulp-babel'),
-	uglify = require('gulp-uglify'),
-	browserSync = require('browser-sync');
+    sass = require('gulp-sass')(require('node-sass')),
+    nano = require('gulp-cssnano'),
+    prefix = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync');
 
-gulp.task('html', function() {
-	return gulp.src('app/index.html')
-		.pipe(gulp.dest('dist/'));
+gulp.task('html', function () {
+    return gulp.src('app/index.html')
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('babel', function() {
-	return gulp.src('app/scripts/script.js')
+gulp.task('babel', function () {
+    return gulp.src('app/scripts/script.js')
         .pipe(babel({
-            presets: ['es2015']
+            presets: ['@babel/env']
         }))
-		.pipe(uglify())
+        .pipe(uglify())
         .pipe(gulp.dest('dist/scripts/'));
 });
 
 gulp.task('sass', function () {
-	return gulp.src('app/styles/master.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
-		.pipe(prefix({
-			browsers: ['> 1%', 'IE 8'], 
+    return gulp.src('app/styles/master.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(prefix({
             cascade: false
-		}))
-		.pipe(nano())
-		.pipe(gulp.dest('dist/styles/'))
-		.pipe(browserSync.stream());
+        }))
+        .pipe(nano())
+        .pipe(gulp.dest('dist/styles/'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('serve', function() {
-	browserSync.init({
-		server: {
-			baseDir: "dist"
-		}
-	});
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
 });
 
 gulp.task('dist', gulp.series('html', 'babel', 'sass'));
-
-gulp.task('default', gulp.series('serve'), function () {
-	gulp.watch('app/*.html', ['html']).on('change', browserSync.reload);
-	gulp.watch("app/scripts/*.js", ['babel']).on('change', browserSync.reload);
-	gulp.watch('app/styles/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('app/*.html', gulp.series('html')); //.on('change', browserSync.reload);
+    gulp.watch("app/scripts/*.js", gulp.series('babel')); //.on('change', browserSync.reload);
+    gulp.watch('app/styles/**/*.scss', gulp.series('sass')); //.on('change', browserSync.reload);
 });
+
+gulp.task('default', gulp.parallel('dist', 'watch', 'serve'));
